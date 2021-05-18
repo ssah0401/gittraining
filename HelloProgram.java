6f74be7ff36084e5130b85516a28fddce4aa4db3
ssah0401 <ssah0401@gmail.com>
2021-05-18 22:11:41 +0530

    Create HelloProgram.java

---
+++ HelloProgram.java
@@ -0,0 +1,8 @@
+public class HelloProgram {	
+	public static void main(String[] s){
+	System.out.println("Loop based execution - CICD Demo- FOR MY MAY BATCH ");
+	
+	for(int i=0; i<50;i++){
+	System.out.println(i);
+	}
+	}
